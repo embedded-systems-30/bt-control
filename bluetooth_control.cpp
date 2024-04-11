e6e5da246cb197c9dcb7fcb857912c7a8336045e
AlyHafez <147519177+AlyHafez@users.noreply.github.com>
2024-04-11 02:20:48 +0100

    Update bluetooth_control.cpp

--- bluetooth_control.cpp
+++ bluetooth_control.cpp
@@ -1,10 +1,5 @@
 #include "mbed.h"
-
-#include "LineSensor.hpp"
-
-
-
-
+#include "LineSensor.hpp"//inclue line sensor class
 
 
 class bluetooth{
@@ -14,47 +9,64 @@ class bluetooth{
    public:
     char c;
 bluetooth(PinName pin, PinName pin2, int Baud_rate): hm10(pin, pin2, Baud_rate){};
-char check_for_data(){
+char check_for_data(){//checks if data is readable then equates character c with the value given to the bluetooth module
 if(hm10.readable()){
     c= hm10.getc();
    return c;
 }
-return '\0';
+return '\0';//if no value is readable return random value in order to showcase error
 }
 };
 
    
 int main(void){
-    PwmOut right_motor(PC_7);
+    PwmOut right_motor(PC_7);//creating objects for bluetooth module and right and left motor
     PwmOut left_motor(PA_8);
     bluetooth bt(PA_11, PA_12, 9600);
-
     bool LineDetected;
-     line_sensor sensor(PA_7, PA_6, PA_5, PA_12, PA_11, PA_8);
-     while(1){
-     LineDetected= sensor.line_detection();
-     bt.check_for_data();
-    if(LineDetected==true){
+     line_sensor sensor(PA_7, PA_6, PA_5, PA_12, PA_11, PA_8);//creating an object for the 6 sensors
 
+     while(1){
+     LineDetected = sensor.line_detection();
+     bt.check_for_data();//check bluetooth module for any new values
+    if(LineDetected==true){//if a line is detected and it is at the centre the buggy will move in a straight line continuously
+        
      if(sensor.line_centre==true){
         right_motor.period_us(30);
         left_motor.period_us(30);
         right_motor.write(0.75f);
-        left_motor.write(0.75f);      
+        left_motor.write(0.75f);
+        bt.check_for_data();
+        printf("line detected");
 
-        if (bt.c=='T'){
-            while(sensor.line_centre==true){
+        if (bt.c=='T'){//if microcontroller recieves command 'T' from buggy it will start doing a uturn until line has been detected again
             right_motor.period_us(30);
             left_motor.period_us(30);
-            right_motor.write(0.5f);
+            right_motor.write(0.25f);
             left_motor.write(0.75f);
+            sensor.line_detection();
+           
             }
-         
         
         }
      }   
-   
+    else if(LineDetected==false&&bt.c=='T') {        //if line not detected and u turn command is still active, continue turning
+            right_motor.period_us(30);
+            left_motor.period_us(30);
+            right_motor.write(0.25f);
+            left_motor.write(0.75f);
+            sensor.line_detection();
+            bt.check_for_data();
+            }
+    else {//if line is not detected and bluetooth command not received, stop the buggy
+            right_motor.period_us(30);
+            left_motor.period_us(30);
+            right_motor.write(0.5f);
+            left_motor.write(0.5f);
+            sensor.line_detection();
+            }
+
+    wait(0.1);
    }
  }
- wait(0.1);
-}
+

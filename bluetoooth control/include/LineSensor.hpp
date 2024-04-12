b31cf35a4f1bc538c5d240bc65af36648e2064c5
AlyHafez <147519177+AlyHafez@users.noreply.github.com>
2024-04-12 17:32:39 +0100

    Add files via upload

---
+++ bluetoooth control/include/LineSensor.hpp
@@ -0,0 +1,52 @@
+
+#include "mbed.h"
+
+
+
+class line_sensor{
+    private:
+    AnalogIn sensor_1, sensor_2, sensor_3, sensor_4, sensor_5, sensor_6;
+    float threshhold;
+   
+    public:
+     bool line_right, line_left, line_centre;
+    line_sensor(PinName s1, PinName s2, PinName s3, PinName s4, PinName s5, PinName s6) : 
+    sensor_1(s1), sensor_2(s2), sensor_3(s3), sensor_4(s4), sensor_5(s5), sensor_6(s6){
+        threshhold = 1.2;
+    }
+    
+    bool line_detection(){
+        float value_1= sensor_1.read();
+        float value_2= sensor_2.read();
+        float value_3= sensor_3.read();
+        float value_4= sensor_4.read();
+        float value_5= sensor_5.read();
+        float value_6= sensor_6.read();
+
+        if(value_1>=threshhold||value_2>=threshhold||value_3>=threshhold||value_4>=threshhold||
+        value_5>=threshhold||value_6>=threshhold){
+            return true;  
+            if(value_1>=threshhold||value_2>=threshhold||(value_2&&value_3>=threshhold)){
+                line_right = true;
+                line_left = false;
+                line_centre = false;
+            }
+            else if((value_3 || value_4)>=threshhold&&(value_2 && value_1 && value_5 && value_6)<threshhold){
+                line_centre = true;
+                line_right = false;
+                line_left = false;
+            }
+            else if(value_6>= threshhold||value_5>=threshhold||(value_5&&value_4)>=threshhold){
+                line_centre =false;
+                line_right = false;
+                line_left = true;
+            }
+        }else{return false;}
+
+
+        }
+
+
+    
+    };
+    
---
+++ bluetoooth control/src/BluetoothControl.cpp
@@ -0,0 +1,73 @@
+
+#include "mbed.h"
+#include "LineSensor.hpp"//inclue line sensor class
+
+
+
+class bluetooth{
+    private:
+    BufferedSerial hm10;
+   
+   public:
+    char c;
+bluetooth(PinName pin, PinName pin2, int Baud_rate): hm10(pin, pin2, Baud_rate){};
+char check_for_data(){//checks if data is readable then equates character c with the value given to the bluetooth module
+if(hm10.readable()){
+    c= hm10.read(&c,1);
+   return c;
+}
+return '\0';//if no value is readable return random value in order to showcase error
+}
+};
+
+   
+int main(void){
+    PwmOut right_motor(PC_7);//creating objects for bluetooth module and right and left motor
+    PwmOut left_motor(PA_8);
+    bluetooth bt(PA_11, PA_12, 9600);
+    bool LineDetected;
+     line_sensor sensor(A0, A1, A2, A3, A4, A5);//creating an object for the 6 sensors
+
+     while(1){
+     LineDetected = sensor.line_detection();
+     bt.check_for_data();//check bluetooth module for any new values
+    if(LineDetected==true){//if a line is detected and it is at the centre the buggy will move in a straight line continuously
+        
+     if(sensor.line_centre==true){
+        right_motor.period_us(30);
+        left_motor.period_us(30);
+        right_motor.write(0.75f);
+        left_motor.write(0.75f);
+        bt.check_for_data();
+        printf("line detected");
+
+        if (bt.c=='T'){//if microcontroller recieves command 'T' from buggy it will start doing a uturn until line has been detected again
+            right_motor.period_us(30);
+            left_motor.period_us(30);
+            right_motor.write(0.25f);
+            left_motor.write(0.75f);
+            sensor.line_detection();
+           
+            }
+        
+        }
+     }   
+    else if(LineDetected==false&&bt.c=='T') {        //if line not detected and u turn command is still active, continue turning
+            right_motor.period_us(30);
+            left_motor.period_us(30);
+            right_motor.write(0.25f);
+            left_motor.write(0.75f);
+            sensor.line_detection();
+            bt.check_for_data();
+            }
+    else {//if line is not detected and bluetooth command not received, stop the buggy
+            right_motor.period_us(30);
+            left_motor.period_us(30);
+            right_motor.write(0.5f);
+            left_motor.write(0.5f);
+            sensor.line_detection();
+            }
+
+    ThisThread::sleep_for(100ms);
+   }
+ }
